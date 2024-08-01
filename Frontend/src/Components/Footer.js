190115bf25ed7515faa61c9682c030cb108800cd
Shivam <you@example.com>
2024-08-01 21:19:24 +0530

    Small changes

--- Frontend/src/Components/Footer.js
+++ Frontend/src/Components/Footer.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../imageComponents/logo.svg";
 import "./css/Footer.css";
 
 export default function Footer() {
@@ -10,9 +8,7 @@ export default function Footer() {
         <div className="footer-section">
           <h4>Company</h4>
           <ul>
-            <li><a href="#careers">Careers</a></li>
-            <li><a href="#events">Events</a></li>
-            <li><a href="#contact-us">Contact us</a></li>
+            <li><a href="/contact">Contact us</a></li>
           </ul>
         </div>
         <div className="footer-section">
@@ -20,18 +16,12 @@ export default function Footer() {
           <ul>
             <li><a href="#product1">TaskTrek Basic</a></li>
             <li><a href="#product2">TaskTrek Pro</a></li>
-            <li><a href="#product3">TaskTrek Enterprise</a></li>
-            <li><a href="#product4">TaskTrek Mobile</a></li>
-            <li><a href="#product5">TaskTrek Desktop</a></li>
           </ul>
         </div>
         <div className="footer-section">
           <h4>Resources</h4>
           <ul>
             <li><a href="/">Technical support</a></li>
-            <li><a href="/">Documentation</a></li>
-            <li><a href="#community">Community</a></li>
-            <li><a href="#my-account">My account</a></li>
           </ul>
         </div>
       </div>
--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
       if (localStorage.getItem("team-user")) {
         navigate("/dashteam");
       }
-      navigate("/login/handler");
+      navigate("/login");
     }
   };
   return (
